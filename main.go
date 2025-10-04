05436c23c6d21f86c4659ae213c997b0198efc6d
Omar Jarbou <omarjarbou2019@gmail.com>
2025-10-04 23:32:19 +0300

    Implement file reading functionality in main.go to read and print contents of messages.txt in 8-byte chunks.

--- main.go
+++ main.go
@@ -2,8 +2,31 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"os"
 )
 
-func main(){
-	fmt.Println("I hope I get the job!")
+func main() {
+	file, err := os.Open("./messages.txt")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	eight_bytes := make([]byte, 8)
+	for {
+		n, err := file.Read(eight_bytes)
+		if n > 0 {
+			fmt.Println("read:", string(eight_bytes[:n]))
+		}
+		if err == io.EOF {
+			os.Exit(0)
+			return
+		}
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+	}
 }
